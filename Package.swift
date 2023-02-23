73637f27632a750ec31d3f0e768a969713c040a4
Github Action <gh-action@lava.ai>
2023-02-23 12:03:51 +0000

    Release version 2.0.13

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.0.12"
-let checksum = "72d077847821ab37c7a238c71e3c3643fa035bf1b95d95474059afd8940c6348"
+let version = "2.0.13"
+let checksum = "f0b25597db758c0b60cdff5bb23e1bc9052dd9b06ff704a52f7b8fbdf1a44449"
 
 let package = Package(
     name: "LavaSDK",
