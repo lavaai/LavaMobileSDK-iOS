c9b9a6a2999f852c1251d89dea12f3496672d24e
Github Action <gh-action@lava.ai>
2025-06-16 11:04:36 +0000

    Update Swift package for 2.0.31

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.0.31-rc1"
-let checksum = "08a5434e41721506bc76072b82d5024d55fdcb03f40fbb86bfe4706e7685c4b8"
+let version = "2.0.31"
+let checksum = "92357e24812e0b5791f2620af6e27c6d640bfbdf5f7b14fdf7541c8fc762257a"
 
 let package = Package(
     name: "LavaSDK",
