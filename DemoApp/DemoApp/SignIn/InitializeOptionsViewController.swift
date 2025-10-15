418c56db1e2224122d50137fe3c0c92f04a85dd6
Thuong Nguyen <thuong.nguyen@lava.ai>
2025-10-15 14:42:51 +0700

    feat: add init options VC for testing purpose

---
+++ DemoApp/DemoApp/SignIn/InitializeOptionsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InitializeOptionsViewController.swift
+//  DemoApp
+//
+//  Created by Thuong Nguyen on 15/10/25.
+//  Copyright © 2025 LAVA. All rights reserved.
+//
+
+import UIKit
+
+class InitializeOptionsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
